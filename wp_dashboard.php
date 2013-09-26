3352d4a53c10441fbde2c1a3c55513c65e64320f
Marius Mandal <mariusmandal@gmail.com>
2013-09-26 14:57:08 +0200

    debug

--- wp_dashboard.php
+++ wp_dashboard.php
@@ -17,6 +17,8 @@ require(ABSPATH . 'wp-admin/admin-header.php');
 $TWIGdata = array('site_type' => get_option('site_type'),
 				  'kontakter' => UKMWP_kontakter());
 
+var_dump($TWIGdata);
+
 echo TWIG('dashboard.twig.html', $TWIGdata, dirname(__FILE__));
 
 require(ABSPATH . 'wp-admin/admin-footer.php');
