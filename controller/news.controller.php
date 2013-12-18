ffc7cc3af1543152fa526b5a0be0bb46123fc8e3
Marius Mandal <mariusmandal@gmail.com>
2013-12-18 13:40:16 +0100

    cleanup

--- controller/news.controller.php
+++ controller/news.controller.php
@@ -6,34 +6,6 @@ $TWIGdata['image_path'] = 'http://arrangor.ukm.no/';
 require_once('WPOO/WPOO/Post.php');
 require_once('WPOO/WPOO/Author.php');
 
-/*
-add_action( 'pre_get_posts', 'blogid_query_set_blog_id' );
-add_action( 'loop_start', 'blogid_query_set_blog_id' );
-add_action( 'loop_end', 'blogid_query_restore_blog_id' );
-add_filter( 'posts_results', 'blogid_query_posts_results', 10, 2 );
-
-
-function blogid_query_set_blog_id( $query ) {
-	global $wpdb;
-	if ( isset($query->query_vars['blogid']) && $query->query_vars['blogid'] != $wpdb->blogid ){
-		$query->original_blog_id = $wpdb->blogid;
-		$wpdb->set_blog_id( $query->query_vars['blogid'] );
-	}
-}
-function blogid_query_restore_blog_id( $query ) {
-	global $wpdb;	
-	if ( isset($query->query_vars['blogid']) && isset($query->original_blog_id) )
-		$wpdb->set_blog_id( $query->original_blog_id );
-}
-
-function blogid_query_posts_results( $posts, $query ) {
-	blogid_query_restore_blog_id( $query );
-	return $posts;
-}
-
-*/
-
-
 switch_to_blog($ID_ARRANGOR);
 if(isset($_GET['post'])) {
 	$posts = query_posts( 'p='.$_GET['post'] );
@@ -55,6 +27,7 @@ if(isset($_GET['post'])) {
 		the_post();
 		$wpoo = new WPOO_Post( $post );
 		
+/*
 		$wpoo->image->url = str_replace('/wp-content/uploads/', 
 										'/wp-content/blogs.dir/'.$ID_ARRANGOR.'/files/',
 										$wpoo->image->url
@@ -64,6 +37,7 @@ if(isset($_GET['post'])) {
 										'/wp-content/blogs.dir/'.$ID_ARRANGOR.'/files/',
 										$wpoo->image->url
 										);
+*/
 		$TWIGdata['news'][] = $wpoo;
 	}
 }
