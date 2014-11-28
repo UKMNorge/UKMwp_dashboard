ac1a7166160327870b1bf9bf4a9d6c3b29dd3db8
Marius Mandal <mariusmandal@gmail.com>
2014-11-28 16:15:21 +0100

    debug

--- controller/network/support.controller.php
+++ controller/network/support.controller.php
@@ -26,9 +26,9 @@ while( $fylke = mysql_fetch_assoc( $fylker ) ) {
 		$lokalmonstring = new stdClass();
 		$lokalmonstring->blog_url = 'http://'. UKM_HOSTNAME .'/pl'. $plid .'/';
 		$lokalmonstring->blog_path = '/pl'.$plid.'/';
-		$lokalmonstring->blog_id = get_blog_id_from_url(UKM_HOSTNAME,$lokalmonstring->blog_path);
-#		echo UKM_HOSTNAME .' - '. $lokalmonstring->blog_path .' <br />';
-#		var_dump( get_blog_id_from_url(UKM_HOSTNAME,$lokalmonstring->blog_path ) );
+		$lokalmonstring->blog_id = get_blog_id_from_url(UKM_HOSTNAME,'/pl3657/');
+		echo UKM_HOSTNAME .' - '. $lokalmonstring->blog_path .' <br />';
+		var_dump( $lokalmonstring->blog_id );
 		$blog_details = get_blog_details( $lokalmonstring->blog_id, 'blogname' );
 		$lokalmonstring->name = $blog_details->blogname;
 		$lokalmonstring->fylke = $fylke->name;
