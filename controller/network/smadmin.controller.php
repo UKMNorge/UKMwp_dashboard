391f83019ce30642a96915ae6c17efed1cc204ef
Asgeir Stavik Hustad <askhus@online.no>
2016-02-15 11:58:48 +0000

    Fix #4, melding til lokalkontakter.

--- controller/network/smadmin.controller.php
+++ controller/network/smadmin.controller.php
@@ -36,22 +36,24 @@ function UKMstimulering_frister() {
 	return $frister;
 }
 
-function dato($time, $format) {
-	if( is_string( $time ) && !is_numeric( $time ) ) {
-		$time = strtotime($time);
+if (!function_exists("dato")) {
+	function dato($time, $format) {
+		if( is_string( $time ) && !is_numeric( $time ) ) {
+			$time = strtotime($time);
+		}
+		$date = date($format, $time);
+		return str_replace(array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday',
+								 'Mon','Tue','Wed','Thu','Fri','Sat','Sun',
+								 'January','February','March','April','May','June',
+								 'July','August','September','October','November','December',
+								 'Jan','Feb','Mar','Apr','May','Jun',
+								 'Jul','Aug','Sep','Oct','Nov','Dec'),
+						  array('mandag','tirsdag','onsdag','torsdag','fredag','lørdag','søndag',
+						  		'man','tir','ons','tor','fre','lør','søn',
+						  		'januar','februar','mars','april','mai','juni',
+						  		'juli','august','september','oktober','november','desember',
+						  		'jan','feb','mar','apr','mai','jun',
+						  		'jul','aug','sep','okt','nov','des'), 
+						  $date);
 	}
-	$date = date($format, $time);
-	return str_replace(array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday',
-							 'Mon','Tue','Wed','Thu','Fri','Sat','Sun',
-							 'January','February','March','April','May','June',
-							 'July','August','September','October','November','December',
-							 'Jan','Feb','Mar','Apr','May','Jun',
-							 'Jul','Aug','Sep','Oct','Nov','Dec'),
-					  array('mandag','tirsdag','onsdag','torsdag','fredag','lørdag','søndag',
-					  		'man','tir','ons','tor','fre','lør','søn',
-					  		'januar','februar','mars','april','mai','juni',
-					  		'juli','august','september','oktober','november','desember',
-					  		'jan','feb','mar','apr','mai','jun',
-					  		'jul','aug','sep','okt','nov','des'), 
-					  $date);
 }
