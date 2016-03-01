f60667059e11d3ede950c306d9c725787549e7b9
Marius Mandal <mariusmandal@gmail.com>
2016-03-01 20:40:25 +0100

    support for everyone
    @AsgeirSH partytime

--- ukmdashboard.php
+++ ukmdashboard.php
@@ -27,7 +27,7 @@ function UKMwpd_menu() {
 		add_action( 'admin_print_styles-' . $subpage2, 'UKMdokumenter_sns' );	
 		add_action( 'admin_print_styles-' . $subpage3, 'UKMWP_support_scriptsandstyles' );	
 	
-		$supportpage = add_submenu_page('index.php', 'Brukerstøtte', 'Brukerstøtte', 'editor', 'UKMwpd_support', 'UKMwpd_support');
+		$supportpage = add_submenu_page('index.php', 'Brukerstøtte', 'Brukerstøtte', 'subscriber', 'UKMwpd_support', 'UKMwpd_support');
 		add_action( 'admin_print_styles-' . $supportpage, 'UKMWP_support_scriptsandstyles' );
 
 		// Add notification about dates!
