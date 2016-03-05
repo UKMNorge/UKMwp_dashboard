40b737a240619a143bccc27961e171c847654b05
Marius Mandal <mariusmandal@gmail.com>
2016-03-05 13:14:32 +0100

    cast to int for sikkerhets skyld

--- controller/network/sveve.controller.php
+++ controller/network/sveve.controller.php
@@ -4,7 +4,7 @@ require_once('UKM/curl.class.php');
 if( UKM_HOSTNAME == 'ukm.no' ) {
 	$curl = new UKMcurl();
 	$res = $curl->request('https://sveve.no/SMS/AccountAdm?cmd=sms_count&user='. UKM_SVEVE_ACCOUNT );
-	$TWIGdata['sveve'] = trim($res);
+	$TWIGdata['sveve'] = (int) $res;
 } else {
 	$TWIGdata['sveve'] = 'N/A';
 }
