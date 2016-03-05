0293fe5a4c186bf9c1d4df497de492664343cf90
Marius Mandal <mariusmandal@gmail.com>
2016-03-05 13:11:29 +0100

    SMS-credits tilgjengelig i admin

--- controller/network/sveve.controller.php
+++ controller/network/sveve.controller.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once('UKM/curl.class.php');
-
-$curl = new UKMcurl();
-$res = $curl->request('https://sveve.no/SMS/AccountAdm?cmd=sms_count&user='. UKM_SVEVE_ACCOUNT );# .'&passwd='. UKM_SVEVE_PASSWORD);
-
-var_dump( $curl );
-
-$TWIGdata['sveve'] = $res;
+if( UKM_HOSTNAME == 'ukm.no' ) {
+	$curl = new UKMcurl();
+	$res = $curl->request('https://sveve.no/SMS/AccountAdm?cmd=sms_count&user='. UKM_SVEVE_ACCOUNT );
+	$TWIGdata['sveve'] = trim($res);
+} else {
+	$TWIGdata['sveve'] = 'N/A';
+}
