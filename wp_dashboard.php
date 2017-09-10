48ba6b8492138c48b699e404645eef9d205a7b1a
Marius Mandal <mariusmandal@gmail.com>
2017-09-10 16:21:55 +0200

    Fjernet kort-adresser pga ny URL-struktur

--- wp_dashboard.php
+++ wp_dashboard.php
@@ -109,10 +109,6 @@ if ($deltakerbruker) {
 	#var_dump(get_avatar($current_user->ID));
 }
 
-/* BRUKER-URLER */
-require_once(__DIR__.'/controller/urls.controller.php');
-$TWIGdata['urls'] = get_urls();
-
 $TWIGdata['user_avatar'] = get_avatar($current_user->ID);
 
 /* NEWS */
