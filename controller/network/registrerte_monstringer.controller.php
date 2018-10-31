de83272164b65f7bf116faa13664b483658af83a
Marius Mandal <mariusmandal@gmail.com>
2018-10-31 16:03:00 +0100

    Bruk ny beregning antall (u)registrerte

--- controller/network/registrerte_monstringer.controller.php
+++ controller/network/registrerte_monstringer.controller.php
@@ -1,9 +1,9 @@
 <?php
 require_once('UKM/monstringer.class.php');
-$monstringer = new monstringer( get_site_option('season') );
+#$monstringer = new monstringer( get_site_option('season') );
 
-$registrert = $monstringer->antall_registrerte();
-$uregistrert = $monstringer->antall_uregistrerte();
+$registrert = stat_monstringer_v2::getAntallUregistrerte( get_site_option('season') );#$monstringer->antall_registrerte();
+$uregistrert = stat_monstringer_v2::getAntallRegistrerte( get_site_option('season') );#$monstringer->antall_uregistrerte();
 
 if( $uregistrert >= $registrert ) {
 	$status = 'danger';
