9e301b1a48ad4924dcaffe79f79b08cf79997c2e
Christian <christian.hvatum.ch@hotmail.com>
2020-01-08 15:53:13 +0100

    Reintrodusert Kommune- og Fylkesvelgeren i Nettverksadmin. + Lagt meldinger i engen blokk i Nettverksadmin
    Close #25 😎

--- controller/network/support.controller.php
+++ controller/network/support.controller.php
@@ -1,22 +1,8 @@
 <?php
-require_once('UKM/monstringer.collection.php');
 
-$TWIGdata['monstringer'] = [];
+use UKMNorge\Geografi\Fylke;
+use UKMNorge\Geografi\Fylker;
 
-$monstringer = new monstringer_v2( get_site_option('season') );
-foreach( $monstringer->getAllBySesong() as $monstring ) {
+require_once('UKM/Autoloader.php');
 
-	$path = '/'. str_replace('//'.UKM_HOSTNAME.'/', '', $monstring->getLink() );
-
-	$data = new stdClass();
-	$data->blog_url 	= $monstring->getLink();
-	$data->blog_id 		= get_blog_id_from_url( UKM_HOSTNAME, $path );
-	$data->name 		= $monstring->getNavn();
-	try {
-		$data->fylke 	= $monstring->getFylke()->getNavn();
-	} catch( Exception $e ) {
-		$data->fylke	= 'Ukjent';
-	}
-
-	$TWIGdata['monstringer'][ $monstring->getType() ][ $path ] = $data;
-}
+$TWIGdata['fylker'] = Fylker::getAll();
