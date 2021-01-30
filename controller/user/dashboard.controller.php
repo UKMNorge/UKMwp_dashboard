eddfc61039e6bae9fad6970825a3d0a2379a3701
Marius Mandal <mariusmandal@gmail.com>
2021-01-30 16:55:19 +0100

    Ekskluder markedsføringsfilmer og nyhetsbrev

--- controller/user/dashboard.controller.php
+++ controller/user/dashboard.controller.php
@@ -1,12 +1,31 @@
 <?php
+
 use UKMNorge\Nettverk\Omrade;
+
 global $current_user;
 
-$TWIGdata['blogs'] = get_blogs_of_user( $current_user->ID );
-$TWIGdata['kurskommune'] = Omrade::getByKommune( UKM_HOSTNAME == 'ukm.dev' ? 5441 : 3303);
+$TWIGdata['blogs'] = get_blogs_of_user($current_user->ID);
+$TWIGdata['kurskommune'] = Omrade::getByKommune(UKM_HOSTNAME == 'ukm.dev' ? 5441 : 3303);
+
+require_once(UKMwp_innhold::getPath() . 'functions/getCategory.function.php');
+
+$category_ids = [];
+
+// Ta ut markedsføringsfilmer
+$categories = [
+    'markedsforing-filmer' => '-',
+    'alle-nyhetsbrev' => '-'
+];
+
+foreach( $categories as $category_slug => $action ) {
+    $category_data = getCategory($category_slug);
+    if( $category_data ) {
+        $category_ids[] = $action . $category_data->term_id;
+    }
+}
 
 /* NEWS */
-$POST_QUERY = 'cat=-2';
-require_once( UKMwp_innhold::getPluginPath() .'controller/news.controller.php');
+$POST_QUERY = 'cat=' . implode(',', $category_ids);
+require_once(UKMwp_innhold::getPluginPath() . 'controller/news.controller.php');
 
 require_once('profil.controller.php');
