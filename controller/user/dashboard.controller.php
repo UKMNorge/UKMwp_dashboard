68843f94e2dbad29c8900a3edb318cd40742e2a1
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-10-17 10:02:43 +0200

    kurs har endret id igjen

--- controller/user/dashboard.controller.php
+++ controller/user/dashboard.controller.php
@@ -5,7 +5,7 @@ use UKMNorge\Nettverk\Omrade;
 global $current_user;
 
 $TWIGdata['blogs'] = get_blogs_of_user($current_user->ID);
-$TWIGdata['kurskommune'] = Omrade::getByKommune(UKM_HOSTNAME == 'ukm.dev' ? 5441 : 3391);
+$TWIGdata['kurskommune'] = Omrade::getByKommune(UKM_HOSTNAME == 'ukm.dev' ? 5441 : 9291);
 
 require_once(UKMwp_innhold::getPath() . 'functions/getCategory.function.php');
 
