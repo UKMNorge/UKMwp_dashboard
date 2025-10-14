028f15cd883514d940647d0c9be61c0f8e232f5b
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-10-14 16:38:51 +0200

    Metoder for å hente fylker og kommuner fra dashboard

--- wp_dashboard.php
+++ wp_dashboard.php
@@ -1,7 +1,16 @@
 <?php
 
+/**
+ * This file is part of the UKMwp_dashboard plugin.
+ *
+ * Contains core functionality for the WordPress dashboard enhancements provided by the plugin.
+ */
+
 use UKMNorge\Nettverk\Administrator;
 use UKMNorge\OAuth2\ArrSys\AccessControlArrSys;
+use UKMNorge\Nettverk\Omrade;
+
+
 
 /* ****************************************************************
 Omkring linje 12 i wp-admin/index.php skal require dashboard
@@ -44,6 +53,9 @@ if( is_user_admin() ) {
 
 	$TWIGdata['user_arrangementer'] = $user_arrangementer;
 	$TWIGdata['isFylkeAdmin'] = AccessControlArrSys::hasFylkeAccess();
+	$TWIGdata['admin_fylker'] = getUserFylker();
+	$TWIGdata['admin_kommuner'] = getUserKommuner();
+
 
 
 	require_once('controller/user/dashboard.controller.php');
@@ -67,4 +79,38 @@ if( is_user_admin() ) {
 
 
 require(ABSPATH . 'wp-admin/admin-footer.php');
-die();
+
+
+
+
+/**
+ * Hent alle fylker brukeren er admin for
+ * @return Omrade[]
+ */
+function getUserFylker() {
+	$retFylker = [];
+	$user = new Administrator( get_current_user_id() );
+
+	foreach($user->getOmrader() as $omrade) {
+		if($omrade->getType() == 'fylke') {
+			$retFylker[] = $omrade;
+		}
+	}
+	return $retFylker;
+}
+
+/** 
+ * Hent alle kommuner brukeren er admin for
+ * @return Omrade[]
+ */
+function getUserKommuner() {
+	$retKommuner = [];
+	$user = new Administrator( get_current_user_id() );
+
+	foreach($user->getOmrader() as $omrade) {
+		if($omrade->getType() == 'kommune') {
+			$retKommuner[] = $omrade;
+		}
+	}
+	return $retKommuner;
+}
