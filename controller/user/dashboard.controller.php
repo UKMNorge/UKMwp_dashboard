724f3b86d3bdb5a080f7ca4706bd4bdc9be22e69
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2026-01-14 23:13:58 +0100

    Henter Teams webinarer på min side

--- controller/user/dashboard.controller.php
+++ controller/user/dashboard.controller.php
@@ -1,12 +1,15 @@
 <?php
 
 use UKMNorge\Nettverk\Omrade;
+use UKMNorge\UKMWebinar\TeamsUKMWebinarer;
 
 global $current_user;
 
 $TWIGdata['blogs'] = get_blogs_of_user($current_user->ID);
 $TWIGdata['kurskommune'] = Omrade::getByKommune(UKM_HOSTNAME == 'ukm.dev' ? 5441 : 9291);
 
+$teamsWebinarer = new TeamsUKMWebinarer();
+$TWIGdata['teamsWebinarer'] = $teamsWebinarer->getAlleAcive();
 require_once(UKMwp_innhold::getPath() . 'functions/getCategory.function.php');
 
 $category_ids = [];
